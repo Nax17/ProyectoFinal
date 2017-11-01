a588587dda276d43c3c8a1b25fa2952754c283fc
aquiles <aquiles@DESKTOP-26T8LTP>
2017-11-01 10:57:43 -0400

    agregando atributos

--- ProyectoFinal/src/logical/Test.java
+++ ProyectoFinal/src/logical/Test.java
@@ -1,5 +1,44 @@
 package logical;
 
 public class Test {
+private int hola;
+private double heyyyy;
+private String yorman;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public int getHola() {
+	return hola;
+}
+public void setHola(int hola) {
+	this.hola = hola;
+}
+public double getHeyyyy() {
+	return heyyyy;
+}
+public void setHeyyyy(double heyyyy) {
+	this.heyyyy = heyyyy;
+}
+public String getYorman() {
+	return yorman;
+}
+public void setYorman(String yorman) {
+	this.yorman = yorman;
+}
 
 }
