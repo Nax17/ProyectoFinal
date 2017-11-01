3cbb2439928373887f98ee9b1c7dcb6550b329f4
luis_ <luis_@DESKTOP-V49J65F>
2017-11-01 11:01:26 -0400

    Creacion de constructor

--- ProyectoFinal/src/logical/Test.java
+++ ProyectoFinal/src/logical/Test.java
@@ -1,44 +1,33 @@
 package logical;
 
 public class Test {
-private int hola;
-private double heyyyy;
-private String yorman;
+	private int hola;
+	private double heyyyy;
+	private String yorman;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-public int getHola() {
-	return hola;
-}
-public void setHola(int hola) {
-	this.hola = hola;
-}
-public double getHeyyyy() {
-	return heyyyy;
-}
-public void setHeyyyy(double heyyyy) {
-	this.heyyyy = heyyyy;
-}
-public String getYorman() {
-	return yorman;
-}
-public void setYorman(String yorman) {
-	this.yorman = yorman;
-}
+	public Test(int hola, double heyyyy, String yorman) {
+		super();
+		this.hola = hola;
+		this.heyyyy = heyyyy;
+		this.yorman = yorman;
+	}
+	public int getHola() {
+		return hola;
+	}
+	public void setHola(int hola) {
+		this.hola = hola;
+	}
+	public double getHeyyyy() {
+		return heyyyy;
+	}
+	public void setHeyyyy(double heyyyy) {
+		this.heyyyy = heyyyy;
+	}
+	public String getYorman() {
+		return yorman;
+	}
+	public void setYorman(String yorman) {
+		this.yorman = yorman;
+	}
 
 }
